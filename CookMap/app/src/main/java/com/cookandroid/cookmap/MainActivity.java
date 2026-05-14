909623b5c727bc87f838a44230dbd9d066409fc4
olivia <qwert45640@gmail.com>
2026-05-14 16:24:16 +0900

    연습문제 13-3_CCTV 지우기

--- CookMap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ CookMap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -4,107 +4,94 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.CameraUpdateFactory;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
     MapFragment mapFrag;
-    Button btnPrev, btnNext;
-    GroundOverlayOptions placeMark;
-    List<String> lines = new ArrayList<>();
-    int placeCount = 0;
+    // 변경
+    GroundOverlay lastOverlay;           // 마지막 오버레이
+    List<GroundOverlay> overlayList = new ArrayList<>();  // 전체 오버레이
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("경기도 으뜸 맛집");
+        setTitle("연습문제 13-7");
 
         mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
 
-        btnPrev = (Button) findViewById(R.id.btnPrev);
-        btnNext = (Button) findViewById(R.id.btnNext);
-
-        readCSV();
-
-        btnPrev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String line = lines.get(placeCount--);
-                if (placeCount < 0)
-                    placeCount = lines.size()-1;
-                String[] tokens = line.split(",");
-                double lat = Double.parseDouble(tokens[0]);
-                double lon = Double.parseDouble(tokens[1]);
-                String restName = tokens[2];
-
-                LatLng point;
-                point = new LatLng(lat, lon);
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
-                placeMark = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.food)).position(point, 500f, 500f);
-                gMap.addGroundOverlay(placeMark);
-                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String line = lines.get(placeCount++);
-                if (placeCount > lines.size()-1)
-                    placeCount = 0;
-                String[] tokens = line.split(",");
-                double lat = Double.parseDouble(tokens[0]);
-                double lon = Double.parseDouble(tokens[1]);
-                String restName = tokens[2];
-
-                LatLng point;
-                point = new LatLng(lat, lon);
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
-                placeMark = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.food)).position(point, 500f, 500f);
-                gMap.addGroundOverlay(placeMark);
-                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
-            }
-        });
-
     }
-
-    public void readCSV() {
-        InputStream inputStream = getResources().openRawResource(R.raw.good_place);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        try {
-            String line;
-            line = reader.readLine();
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
-            }
-            reader.close();
-        } catch (Exception e) {}
-    }
-
     @Override
     public void onMapReady(GoogleMap map) {
         gMap = map;
-        gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
         gMap.getUiSettings().setZoomControlsEnabled(true);
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng point) {
+                GroundOverlayOptions videoMark = new GroundOverlayOptions()
+                        .image(BitmapDescriptorFactory.fromResource(R.drawable.presence_video_busy))
+                        .position(point, 400f, 400f);
+
+                lastOverlay = gMap.addGroundOverlay(videoMark);  // 마지막 저장
+                overlayList.add(lastOverlay);                    // 전체 리스트에 추가
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 1, 0, "위성 지도");
+        menu.add(0, 2, 0, "일반 지도");
+        menu.add(0, 3, 0, "바로전 CCTV 지우기");
+        menu.add(0, 4, 0, "모든 CCTV 지우기");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case 2:
+                gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case 3:
+                // 바로 전 CCTV 지우기
+                if (lastOverlay != null) {
+                    lastOverlay.remove();
+                    overlayList.remove(lastOverlay);
+                    lastOverlay = null;
+                }
+                return true;
+
+            case 4:
+                // 모든 CCTV 지우기
+                for (GroundOverlay overlay : overlayList) {
+                    overlay.remove();
+                }
+                overlayList.clear();
+                lastOverlay = null;
+                return true;
+        }
+        return false;
     }
 }
